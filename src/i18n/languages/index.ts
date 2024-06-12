6abd3d5caa191983ad150a85a2548d959ece5c25
Victor Alves <viictor.alvess12@gmail.com>
2024-06-12 18:56:53 -0300

    refactor: change way to extend dayjs locale

--- src/i18n/languages/index.ts
+++ src/i18n/languages/index.ts
@@ -1,12 +1,22 @@
 import { english } from "./en"
 import { brazilianPortuguese } from "./pt-BR"
 
-export const currentLanguages = [
-  { nativeName: 'English (US)', langCode: 'en-US', flag: 'US' },
-  { nativeName: 'Português (Brasil)', langCode: 'pt-BR', flag: 'BR' },
-]
-
 export const languages = {
   'en-US': english,
   'pt-BR': brazilianPortuguese
 }
+
+export const currentLanguages = [
+  {
+    nativeName: 'English (US)',
+    langCode: 'en-US',
+    flag: 'US',
+    dayjsLocale: 'en',
+  },
+  {
+    nativeName: 'Português (Brasil)',
+    langCode: 'pt-BR',
+    flag: 'BR',
+    dayjsLocale: 'pt-br',
+  },
+]
--- src/utils/dateTime.ts
+++ src/utils/dateTime.ts
@@ -2,19 +2,19 @@ import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import updateLocale from 'dayjs/plugin/updateLocale'
-import 'dayjs/locale/pt-BR'
+import 'dayjs/locale/pt-br'
 
 import i18n from '@/i18n'
-
-import { english } from '@/i18n/languages/en'
-import { brazilianPortuguese } from '@/i18n/languages/pt-BR'
+import { currentLanguages, languages } from '@/i18n/languages'
 
 dayjs.extend(duration)
 dayjs.extend(relativeTime)
 dayjs.extend(updateLocale)
 
-dayjs.updateLocale('en', english.translation.dayjs)
-dayjs.updateLocale('pt-br', brazilianPortuguese.translation.dayjs)
+currentLanguages.forEach(lang => {
+  const langCode = lang.langCode as keyof typeof languages
+  dayjs.updateLocale(lang.dayjsLocale, languages[langCode].translation.dayjs)
+})
 
 const savedLang = localStorage.getItem('i18nextLng') || 'en'
 dayjs.locale(savedLang)
