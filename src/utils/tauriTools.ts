6e9f10894585b097233dd457b42ddcf5924d2a51
Victor Alves <viictor.alvess12@gmail.com>
2024-07-04 22:05:18 -0300

    chore: declare __TAURI__ type to isTauri function

--- src/utils/tauriTools.ts
+++ src/utils/tauriTools.ts
@@ -1,3 +1,9 @@
+declare global {
+  interface Window {
+    __TAURI__: Record<string, unknown>
+  }
+}
+
 export function isTauri(): boolean {
   return (
     typeof window !== 'undefined' && typeof window.__TAURI__ !== 'undefined'
