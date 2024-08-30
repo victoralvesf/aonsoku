c270b1c96fbcb57ae4080ea957bdb0c5e244d4ac
Victor Alves <viictor.alvess12@gmail.com>
2024-08-30 16:07:39 -0300

    fix: remove page header shadows

--- src/app/components/fallbacks/ui-fallbacks.tsx
+++ src/app/components/fallbacks/ui-fallbacks.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/app/components/ui/skeleton'
 
 export function ShadowHeaderFallback() {
   return (
-    <div className="flex items-center justify-start px-8 h-[--shadow-header-height] border-b shadow-lg shadow-b-2 -shadow-spread-2">
+    <div className="flex items-center justify-start px-8 h-[--shadow-header-height] border-b bg-background">
       <Skeleton className="w-28 h-8" />
       <Skeleton className="w-11 h-[22px] ml-2 rounded-full" />
     </div>
