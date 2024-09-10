27edda18119577cfa63dd8a3a474016f62a233ed
Victor Alves <viictor.alvess12@gmail.com>
2024-09-09 21:51:29 -0300

    feat: add a new custom variant to badges

--- src/app/components/ui/badge.tsx
+++ src/app/components/ui/badge.tsx
@@ -15,6 +15,8 @@ const badgeVariants = cva(
         destructive:
           'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
         outline: 'text-foreground',
+        neutral:
+          'border-transparent bg-foreground text-background hover:bg-foreground/80',
       },
     },
     defaultVariants: {
