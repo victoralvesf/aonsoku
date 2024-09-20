5a66877422825bd287b5974592ae21b51f3b8af5
victoralvesf <viictor.alvess12@gmail.com>
2024-09-20 16:37:08 -0300

    fix: change keyboard font size on shortcuts modal

--- src/app/components/shortcuts/dialog.tsx
+++ src/app/components/shortcuts/dialog.tsx
@@ -23,13 +23,10 @@ export function ShortcutsDialog({ open, onOpenChange }: ShortcutsDialogProps) {
         <DialogHeader className="p-6 border-b-[1px] border-b-border mb-0">
           <DialogTitle>{t('shortcuts.modal.title')}</DialogTitle>
           <div className="flex gap-1 text-sm text-muted-foreground">
-            <p>{t('shortcuts.modal.description.first')}</p>{' '}
-            <Keyboard
-              text="⌘"
-              className="ml-3 relative w-5 top-0 text-base pl-[4px] pt-[1px]"
-            />{' '}
-            <Keyboard text="/" className="relative w-5 top-0" />
-            <p className="-ml-1">{t('shortcuts.modal.description.last')}</p>
+            <p>{t('shortcuts.modal.description.first')}</p>
+            <Keyboard text="⌘" className="relative top-0 right-0" />
+            <Keyboard text="/" className="relative top-0 right-0" />
+            <p>{t('shortcuts.modal.description.last')}</p>
           </div>
         </DialogHeader>
         <div className="px-6 pt-4 pb-6 max-h-[400px] overflow-y-auto space-y-4">
