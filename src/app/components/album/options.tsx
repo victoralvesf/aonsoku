b49d21eb37d583d516d30d9784d2d155b3864fa3
Victor Alves <viictor.alvess12@gmail.com>
2024-10-08 18:36:54 -0300

    feat: include add to playlist to album more options

--- src/app/components/album/options.tsx
+++ src/app/components/album/options.tsx
@@ -1,12 +1,16 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { getDownloadUrl } from '@/api/httpClient'
 import { OptionsButtons } from '@/app/components/options/buttons'
+import { AddToPlaylistSubMenu } from '@/app/components/song/add-to-playlist'
 import {
   DropdownMenuGroup,
   DropdownMenuSeparator,
 } from '@/app/components/ui/dropdown-menu'
 import { useDownload } from '@/app/hooks/use-download'
+import { subsonic } from '@/service/subsonic'
 import { usePlayerActions } from '@/store/player.store'
 import { SingleAlbum } from '@/types/responses/album'
+import { queryKeys } from '@/utils/queryKeys'
 import { isTauri } from '@/utils/tauriTools'
 
 interface AlbumOptionsProps {
@@ -34,12 +38,55 @@ export function AlbumOptions({ album }: AlbumOptionsProps) {
     }
   }
 
+  const queryClient = useQueryClient()
+
+  const updateMutation = useMutation({
+    mutationFn: subsonic.playlists.update,
+  })
+
+  async function handleAddToPlaylist(id: string) {
+    const albumSongsIds = album.song.map((song) => song.id)
+
+    await updateMutation.mutateAsync({
+      playlistId: id,
+      songIdToAdd: albumSongsIds,
+    })
+  }
+
+  const createMutation = useMutation({
+    mutationFn: subsonic.playlists.createWithDetails,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.playlist.all],
+      })
+    },
+  })
+
+  async function handleCreateNewPlaylist() {
+    const albumSongsIds = album.song.map((song) => song.id)
+
+    await createMutation.mutateAsync({
+      name: album.name,
+      comment: '',
+      isPublic: 'false',
+      songIdToAdd: albumSongsIds,
+    })
+  }
+
   return (
     <>
       <DropdownMenuGroup>
         <OptionsButtons.PlayNext onClick={() => handlePlayNext()} />
         <OptionsButtons.PlayLast onClick={() => handlePlayLast()} />
       </DropdownMenuGroup>
+      <DropdownMenuSeparator />
+      <OptionsButtons.AddToPlaylistOption variant="dropdown">
+        <AddToPlaylistSubMenu
+          type="dropdown"
+          newPlaylistFn={handleCreateNewPlaylist}
+          addToPlaylistFn={handleAddToPlaylist}
+        />
+      </OptionsButtons.AddToPlaylistOption>
       <DropdownMenuSeparator />
       <DropdownMenuGroup>
         <OptionsButtons.Download onClick={() => handleDownload()} />
