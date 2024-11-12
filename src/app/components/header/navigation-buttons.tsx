d970e93455194a0c5572977071c64c05211e2f3e
victoralvesf <viictor.alvess12@gmail.com>
2024-11-12 16:34:57 -0300

    feat: add cursor not allowed if navigation button is disabled

--- src/app/components/header/navigation-buttons.tsx
+++ src/app/components/header/navigation-buttons.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 import { Button } from '@/app/components/ui/button'
@@ -9,24 +10,28 @@ export function NavigationButtons() {
 
   return (
     <div className="flex gap-1">
-      <Button
-        variant="ghost"
-        size="sm"
-        className="h-8 w-8 p-0 rounded-md"
-        disabled={!canGoBack}
-        onClick={() => navigate(-1)}
-      >
-        <ChevronLeft className="w-6 h-6" strokeWidth={1.5} />
-      </Button>
-      <Button
-        variant="ghost"
-        size="sm"
-        className="h-8 w-8 p-0 rounded-md"
-        disabled={!canGoForward}
-        onClick={() => navigate(1)}
-      >
-        <ChevronRight className="w-6 h-6" strokeWidth={1.5} />
-      </Button>
+      <div className={clsx(!canGoBack && 'cursor-not-allowed')}>
+        <Button
+          variant="ghost"
+          size="sm"
+          className="h-8 w-8 p-0 rounded-md disabled:cursor-not-allowed"
+          disabled={!canGoBack}
+          onClick={() => navigate(-1)}
+        >
+          <ChevronLeft className="w-6 h-6" strokeWidth={1.5} />
+        </Button>
+      </div>
+      <div className={clsx(!canGoForward && 'cursor-not-allowed')}>
+        <Button
+          variant="ghost"
+          size="sm"
+          className="h-8 w-8 p-0 rounded-md"
+          disabled={!canGoForward}
+          onClick={() => navigate(1)}
+        >
+          <ChevronRight className="w-6 h-6" strokeWidth={1.5} />
+        </Button>
+      </div>
     </div>
   )
 }
