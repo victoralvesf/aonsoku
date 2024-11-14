9d9597bc0c80f3f725296e4a9fd0843f6a4858e2
victoralvesf <viictor.alvess12@gmail.com>
2024-11-13 22:38:04 -0300

    feat: add search box to songlist page

--- src/app/pages/songs/songlist.tsx
+++ src/app/pages/songs/songlist.tsx
@@ -1,8 +1,10 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
 import { Loader2 } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
+import { useSearchParams } from 'react-router-dom'
 import { ShadowHeader } from '@/app/components/album/shadow-header'
 import { InfinitySongListFallback } from '@/app/components/fallbacks/song-fallbacks'
+import { ExpandableSearchInput } from '@/app/components/search/expandable-input'
 import { Badge } from '@/app/components/ui/badge'
 import { DataTableList } from '@/app/components/ui/data-table-list'
 import { useTotalSongs } from '@/app/hooks/use-total-songs'
@@ -11,20 +13,29 @@ import { subsonic } from '@/service/subsonic'
 import { usePlayerActions } from '@/store/player.store'
 import { ColumnFilter } from '@/types/columnFilter'
 import { queryKeys } from '@/utils/queryKeys'
+import { SearchParamsHandler } from '@/utils/searchParamsHandler'
 
 const DEFAULT_OFFSET = 250
 
 export default function SongList() {
   const { t } = useTranslation()
   const { setSongList } = usePlayerActions()
+  const [searchParams] = useSearchParams()
+  const { getSearchParam } = new SearchParamsHandler(searchParams)
   const columns = songsColumns()
+
+  const filter = getSearchParam<string>('filter', '')
+  const query = getSearchParam<string>('query', '')
+
+  const searchFilterIsSet = filter === 'search' && query !== ''
 
   async function fetchSongs({ pageParam = 0 }) {
     const response = await subsonic.search.get({
+      query: searchFilterIsSet ? query : '',
       artistCount: 0,
       albumCount: 0,
-      songCount: DEFAULT_OFFSET,
-      songOffset: pageParam,
+      songCount: searchFilterIsSet ? 500 : DEFAULT_OFFSET,
+      songOffset: searchFilterIsSet ? 0 : pageParam,
     })
 
     let nextOffset = null
@@ -39,19 +50,21 @@ export default function SongList() {
   }
 
   const { data, isLoading, fetchNextPage, hasNextPage } = useInfiniteQuery({
-    queryKey: [queryKeys.song.all],
+    queryKey: [queryKeys.song.all, filter, query],
     initialPageParam: 0,
     queryFn: fetchSongs,
     getNextPageParam: (lastPage) => lastPage.nextOffset,
   })
 
   const { data: songCountData, isLoading: songCountIsLoading } = useTotalSongs()
-  const songCount = songCountData ?? 0
 
   if (isLoading) return <InfinitySongListFallback />
   if (!data) return null
 
   const songlist = data.pages.flatMap((page) => page.songs) ?? []
+  const songCount = searchFilterIsSet
+    ? songlist.length ?? 0
+    : songCountData ?? 0
 
   function handlePlaySong(index: number) {
     if (songlist) setSongList(songlist, index)
@@ -71,7 +84,7 @@ export default function SongList() {
 
   return (
     <div className="w-full h-app-screen">
-      <ShadowHeader className="relative w-full top-0 left-0 lg:left-0 justify-between">
+      <ShadowHeader className="relative w-full top-0 left-0 lg:left-0 justify-between items-center">
         <div className="flex gap-2 items-center">
           <h2 className="text-2xl font-semibold tracking-tight">
             {t('sidebar.songs')}
@@ -83,6 +96,10 @@ export default function SongList() {
               songCount
             )}
           </Badge>
+        </div>
+
+        <div className="flex gap-2 flex-1 justify-end">
+          <ExpandableSearchInput placeholder="Search for songs..." />
         </div>
       </ShadowHeader>
 
