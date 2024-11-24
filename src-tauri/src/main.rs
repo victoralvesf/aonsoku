5c684232b7e8d236f3a5fbd5968e78770847aa19
Victor Alves <viictor.alvess12@gmail.com>
2024-11-24 20:51:33 -0300

    feat: add tauri update dialog

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -19,48 +19,35 @@ mod commands;
 mod progress;
 mod utils;
 
-#[cfg(target_os = "macos")]
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            use mac::window::setup_traffic_light_positioner;
+        .setup(|_app| {
+            #[cfg(target_os = "macos")]
+            {
+                use mac::window::setup_traffic_light_positioner;
+    
+                let window = _app.get_window("main").unwrap();
+                let window_ = window.clone();
+    
+                window.on_window_event(move |event| {
+                    if let tauri::WindowEvent::ThemeChanged(_theme) = event {
+                        setup_traffic_light_positioner(window_.clone())
+                    }
+                });
 
-            let window = app.get_window("main").unwrap();
-            let window_ = window.clone();
+                let _ = _app.handle().plugin(mac::window::init());
+            }
 
-            window.on_window_event(move |event| {
-                if let tauri::WindowEvent::ThemeChanged(_theme) = event {
-                    setup_traffic_light_positioner(window_.clone())
-                }
-            });
-
+            #[cfg(target_os = "windows")]
+            {
+                let main_window = _app.get_webview_window("main").unwrap();
+                let _ = main_window.set_decorations(false);
+            }
+            
             Ok(())
         })
-        .plugin(mac::window::init())
-        .plugin(tauri_plugin_os::init())
-        .invoke_handler(tauri::generate_handler![commands::download_file])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
-
-#[cfg(target_os = "windows")]
-fn main() {
-    tauri::Builder::default()
-        .setup(|app| {
-            let main_window = app.get_webview_window("main").unwrap();
-            let _ = main_window.set_decorations(false);
-
-            Ok(())
-        })
-        .plugin(tauri_plugin_os::init())
-        .invoke_handler(tauri::generate_handler![commands::download_file])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
-
-#[cfg(target_os = "linux")]
-fn main() {
-    tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_os::init())
         .invoke_handler(tauri::generate_handler![commands::download_file])
         .run(tauri::generate_context!())
--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { RouterProvider } from 'react-router-dom'
 import { LangObserver } from '@/app/observers/lang-observer'
 import { ThemeObserver } from '@/app/observers/theme-observer'
 import { ToastContainer } from '@/app/observers/toast-container'
+import { UpdateObserver } from '@/app/observers/update-observer'
 import { router } from '@/routes/router'
+import { isTauri } from '@/utils/tauriTools'
 
 function App() {
   return (
     <>
+      {isTauri() && <UpdateObserver />}
       <LangObserver />
       <ThemeObserver />
       <RouterProvider router={router} />
