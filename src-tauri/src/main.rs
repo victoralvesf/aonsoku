50dcd0ef4f5d1ceeef16a1d7b61029096ed1612a
Victor Alves <viictor.alvess12@gmail.com>
2024-11-25 12:49:02 -0300

    feat: allow to open external links with default browser

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -20,22 +20,25 @@ mod progress;
 mod utils;
 
 fn main() {
-    tauri::Builder::default()
+    let builder = tauri::Builder::default();
+
+    #[cfg(target_os = "macos")]
+    let builder = builder.plugin(mac::window::init());
+
+    builder
         .setup(|_app| {
             #[cfg(target_os = "macos")]
             {
                 use mac::window::setup_traffic_light_positioner;
-    
+
                 let window = _app.get_window("main").unwrap();
                 let window_ = window.clone();
-    
+
                 window.on_window_event(move |event| {
                     if let tauri::WindowEvent::ThemeChanged(_theme) = event {
                         setup_traffic_light_positioner(window_.clone())
                     }
                 });
-
-                let _ = _app.handle().plugin(mac::window::init());
             }
 
             #[cfg(target_os = "windows")]
@@ -43,9 +46,10 @@ fn main() {
                 let main_window = _app.get_webview_window("main").unwrap();
                 let _ = main_window.set_decorations(false);
             }
-            
+
             Ok(())
         })
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_os::init())
