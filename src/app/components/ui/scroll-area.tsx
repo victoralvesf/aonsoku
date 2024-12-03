4b7e652b9c74d04c2a9f4f6093f78cb567356d30
victoralvesf <viictor.alvess12@gmail.com>
2024-12-03 20:14:18 -0300

    fix: add z-index to custom scrollbar so it appears at the top

--- src/app/components/ui/scroll-area.tsx
+++ src/app/components/ui/scroll-area.tsx
@@ -32,7 +32,7 @@ const ScrollBar = React.forwardRef<
     ref={ref}
     orientation={orientation}
     className={cn(
-      'flex touch-none select-none transition-colors',
+      'flex touch-none select-none transition-colors z-30',
       orientation === 'vertical' &&
         'h-full w-2.5 border-l border-l-transparent p-[1px]',
       orientation === 'horizontal' &&
