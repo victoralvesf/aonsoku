c611fdf6b5e99c5eafede6e35c0f74f8c7a66241
Victor Alves <viictor.alvess12@gmail.com>
2024-12-08 20:09:42 -0300

    feat: change queue height to match app content dimensions

--- src/app/components/header/navigation-buttons.tsx
+++ src/app/components/header/navigation-buttons.tsx
@@ -2,9 +2,11 @@ import clsx from 'clsx'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { Button } from '@/app/components/ui/button'
 import useNavigationHistory from '@/app/hooks/use-navigation-history'
+import { useQueueDrawerState } from '@/store/player.store'
 
 export function NavigationButtons() {
   const { canGoBack, canGoForward, goBack, goForward } = useNavigationHistory()
+  const queueDrawerState = useQueueDrawerState()
 
   return (
     <div className="flex gap-1">
@@ -13,7 +15,7 @@ export function NavigationButtons() {
           variant="ghost"
           size="sm"
           className="h-8 w-8 p-0 rounded-md"
-          disabled={!canGoBack}
+          disabled={!canGoBack || queueDrawerState}
           onClick={goBack}
         >
           <ChevronLeft className="w-6 h-6" strokeWidth={1.5} />
@@ -24,7 +26,7 @@ export function NavigationButtons() {
           variant="ghost"
           size="sm"
           className="h-8 w-8 p-0 rounded-md"
-          disabled={!canGoForward}
+          disabled={!canGoForward || queueDrawerState}
           onClick={goForward}
         >
           <ChevronRight className="w-6 h-6" strokeWidth={1.5} />
