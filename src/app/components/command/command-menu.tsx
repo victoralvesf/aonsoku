a2f41d2e22aec9f8f6e820856a24c502f346a75c
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-01-21 14:12:42 +0300

    fix: Use byte length for search check
    Co-Authored-By: SamsidParty <business@samsidparty.com>

--- src/app/components/command/command-menu.tsx
+++ src/app/components/command/command-menu.tsx
@@ -39,6 +39,7 @@ import {
   CustomHeaderLink,
 } from './command-group'
 import { CustomCommandItem } from './command-item'
+import { byteLength } from '@/utils/byteLength'
 
 type CommandPages = 'HOME' | 'GOTO' | 'THEME' | 'PLAYLISTS' | 'SERVER'
 
@@ -68,7 +69,7 @@ export default function CommandMenu() {
   const activePage = pages[pages.length - 1]
   const isHome = activePage === 'HOME'
 
-  const enableQuery = Boolean(query.length >= 3 && activePage !== 'PLAYLISTS')
+  const enableQuery = Boolean(byteLength(query) >= 3 && activePage !== 'PLAYLISTS')
 
   const { data: searchResult } = useQuery({
     queryKey: [queryKeys.search, query],
---
+++ src/utils/byteLength.ts
@@ -0,0 +1,10 @@
+export function byteLength(str: String) {
+    var s = str.length;
+    for (var i=str.length-1; i>=0; i--) {
+      var code = str.charCodeAt(i);
+      if (code > 0x7f && code <= 0x7ff) s++;
+      else if (code > 0x7ff && code <= 0xffff) s+=2;
+      if (code >= 0xDC00 && code <= 0xDFFF) i--;
+    }
+    return s;
+}
