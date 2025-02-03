fa41ae9e6fc0f778ac011ae42959df46bbe377a5
Victor Alves <viictor.alvess12@gmail.com>
2025-02-03 10:11:02 -0300

    feat: add podcasts services and types

---
+++ src/service/podcasts.ts
@@ -0,0 +1,110 @@
+import { QueryType } from '@/api/httpClient'
+import { podcastClient } from '@/api/podcastClient'
+import {
+  Episode,
+  Podcasts,
+  PodcastWithEpisodes,
+} from '@/types/responses/podcasts'
+
+interface SearchParams {
+  query: string
+  per_page?: number
+  page?: number
+  filter_by?: 'title' | 'description' | 'both'
+}
+
+async function search(params: SearchParams) {
+  const query: QueryType = { ...params }
+
+  const response = await podcastClient<Podcasts>('/podcasts/search', {
+    method: 'GET',
+    query,
+  })
+
+  return response
+}
+
+interface GetAllParams {
+  order_by?: 'title' | 'episode_count'
+  sort?: 'asc' | 'desc'
+  per_page?: number
+  page?: number
+}
+
+async function getAll(params?: GetAllParams) {
+  const query: QueryType = { ...params }
+
+  const response = await podcastClient<Podcasts>('/podcasts', {
+    method: 'GET',
+    query,
+  })
+
+  return response
+}
+
+interface ShowParams {
+  order_by?: 'published_at' | 'title' | 'duration'
+  sort?: 'asc' | 'desc'
+  per_page?: number
+  page?: number
+}
+
+async function getOne(id: string, params?: ShowParams) {
+  const query: QueryType = params ? { ...params } : {}
+
+  const response = await podcastClient<PodcastWithEpisodes>(`/podcasts/${id}`, {
+    method: 'GET',
+    query,
+  })
+
+  return response
+}
+
+type CreateParams =
+  | { feed_url: string; feed_urls?: never }
+  | { feed_urls: string[]; feed_url?: never }
+
+async function create(body: CreateParams) {
+  const response = await podcastClient<PodcastWithEpisodes>('/podcasts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+
+  return response
+}
+
+async function unfollow(id: string) {
+  try {
+    await podcastClient(`/podcasts/${id}`, {
+      method: 'DELETE',
+    })
+
+    return { success: true }
+  } catch (error) {
+    if (error instanceof Error) {
+      return { success: false, error: error.message }
+    }
+
+    return { success: false, error: 'An unexpected error occurred' }
+  }
+}
+
+async function getEpisode(id: string) {
+  const response = await podcastClient<Episode>(`/episodes/${id}`, {
+    method: 'GET',
+  })
+
+  return response
+}
+
+export const podcasts = {
+  getAll,
+  getOne,
+  create,
+  unfollow,
+  search,
+  getEpisode,
+}
