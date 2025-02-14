5cb2ad7ccd21eaace01c07806469ae7a48ddb65a
Victor Alves <viictor.alvess12@gmail.com>
2025-02-14 18:02:35 -0300

    feat: add create podcast form dialog

---
+++ src/app/components/podcasts/form-dialog.tsx
@@ -0,0 +1,126 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import { toast } from 'react-toastify'
+import { z } from 'zod'
+import { Button } from '@/app/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/app/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/app/components/ui/form'
+import { Input } from '@/app/components/ui/input'
+import { podcasts } from '@/service/podcasts'
+import { queryKeys } from '@/utils/queryKeys'
+
+const podcastSchema = z.object({
+  feedUrl: z
+    .string()
+    .url({ message: 'radios.form.validations.url' })
+    .min(10, { message: 'radios.form.validations.streamUrlLength' })
+    .refine((value) => /^https?:\/\//.test(value), {
+      message: 'login.form.validations.protocol',
+    }),
+})
+
+type PodcastSchema = z.infer<typeof podcastSchema>
+
+interface PodcastFormDialogProps {
+  open: boolean
+  setOpen: (value: boolean) => void
+}
+
+export function PodcastFormDialog({ open, setOpen }: PodcastFormDialogProps) {
+  const { t } = useTranslation()
+
+  const form = useForm<PodcastSchema>({
+    resolver: zodResolver(podcastSchema),
+    defaultValues: {
+      feedUrl: '',
+    },
+  })
+
+  const queryClient = useQueryClient()
+
+  const createMutation = useMutation({
+    mutationFn: podcasts.create,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.podcast.all],
+      })
+      toast.success(t('podcasts.form.toasts.success'))
+    },
+    onError: () => {
+      toast.error(t('podcasts.form.toasts.error'))
+    },
+  })
+
+  async function onSubmit({ feedUrl }: PodcastSchema) {
+    await createMutation.mutate({
+      feed_urls: [feedUrl],
+    })
+
+    setOpen(false)
+    form.reset({ feedUrl: '' })
+  }
+
+  return (
+    <Dialog
+      defaultOpen={false}
+      open={open}
+      onOpenChange={(value) => {
+        setOpen(value)
+        form.reset({ feedUrl: '' })
+      }}
+    >
+      <DialogContent className="max-w-[500px]">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <DialogHeader>
+              <DialogTitle>{t('podcasts.form.dialog.title')}</DialogTitle>
+            </DialogHeader>
+            <div className="my-4 space-y-4">
+              <FormField
+                control={form.control}
+                name="feedUrl"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="required">
+                      {t('podcasts.form.dialog.feedUrl')}
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        id="feed-url"
+                        autoCorrect="false"
+                        autoCapitalize="false"
+                        spellCheck="false"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter>
+              <Button type="submit">
+                {t('podcasts.form.dialog.saveButton')}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- src/app/components/podcasts/header.tsx
+++ src/app/components/podcasts/header.tsx
@@ -1,12 +1,15 @@
 import { PlusIcon } from 'lucide-react'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ShadowHeader } from '@/app/components/album/shadow-header'
 import { HeaderTitle } from '@/app/components/header-title'
 import { Button } from '@/app/components/ui/button'
+import { PodcastFormDialog } from './form-dialog'
 import { PodcastsFilters } from './podcasts-filters'
 
 export function PodcastsHeader() {
   const { t } = useTranslation()
+  const [openFormDialog, setOpenFormDialog] = useState(false)
 
   return (
     <ShadowHeader>
@@ -20,13 +23,15 @@ export function PodcastsHeader() {
             size="sm"
             variant="default"
             className="px-4"
-            onClick={() => {}}
+            onClick={() => setOpenFormDialog(true)}
           >
             <PlusIcon className="w-5 h-5 -ml-[3px]" />
             <span className="ml-2">{t('podcasts.form.addButton')}</span>
           </Button>
         </div>
       </div>
+
+      <PodcastFormDialog open={openFormDialog} setOpen={setOpenFormDialog} />
     </ShadowHeader>
   )
 }
