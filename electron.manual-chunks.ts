5ecbf6ca342f265c91a7285627b8e7abaed47dd8
Victor Alves <viictor.alvess12@gmail.com>
2025-04-13 18:50:43 -0300

    chore: improve manual chunks

--- electron.manual-chunks.ts
+++ electron.manual-chunks.ts
@@ -1,5 +1,5 @@
 export function createElectronManualChunks(id: string) {
-  const vendor = ['react', 'react-dom', 'react-router-dom']
+  const vendor = ['react-dom', 'react-router-dom']
 
   if (id.includes('node_modules')) {
     const modulePath = id.split('node_modules/')[1]
@@ -12,15 +12,23 @@ export function createElectronManualChunks(id: string) {
     const scopedPackageName = modulePath.split('/')[1]
     const includes = (id: string) => scopedPackageName.includes(id)
 
-    if (vendor.some((name) => scopedPackageName.startsWith(name))) {
-      return 'vendor'
-    }
     if (includes('i18n')) return 'i18n'
     if (includes('tailwind')) return 'tailwind'
-    if (includes('electron')) return 'electron'
-    if (includes('@tanstack')) return 'tanstack'
-    if (includes('@radix-ui')) return 'radix'
+    if (includes('html-to-text') || includes('linkify-it')) return 'formatters'
+    if (includes('lucide')) return 'lucide'
+    if (includes('embla')) return 'embla'
+    if (includes('dayjs')) return 'date-time'
+    if (includes('audio-context')) return 'audio-context'
+    if (includes('crypto')) return 'crypto'
+    if (includes('lodash')) return 'lodash'
+    if (includes('tanstack')) return 'tanstack'
+    if (includes('radix')) return 'radix'
     if (includes('markdown') || includes('remark')) return 'markdown'
+    if (includes('react-hook-form') || includes('zod')) return 'forms'
+    if (vendor.some((name) => scopedPackageName.startsWith(name))) {
+      return 'vendor-dom'
+    }
+    if (scopedPackageName.startsWith('react')) return 'vendor'
 
     return undefined
   }
