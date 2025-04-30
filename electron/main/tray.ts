22565c0b99825a1d252049afa4755aefb57ab6d9
Victor Alves <viictor.alvess12@gmail.com>
2025-04-30 00:30:34 -0300

    feat: add better icons to tray menu

--- electron/main/tray.ts
+++ electron/main/tray.ts
@@ -1,19 +1,99 @@
-import { app, Menu, Tray } from 'electron'
+import { platform } from '@electron-toolkit/utils'
+import { app, Menu, NativeImage, nativeImage, screen, Tray } from 'electron'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 import { mainWindow } from './app'
 import { aboutDialog } from './core/about'
-import { appIcon } from './core/icon'
 import { sendPlayerEvents } from './core/playerEvents'
 import { playerState } from './core/playerState'
+import { resourcesPath } from './core/taskbar'
 import { productName } from '../../package.json'
 
 const traySpacer = Array.from({ length: 30 }).join(' ')
 
 export let tray: Tray | null = null
 
-export function createTray() {
-  tray = new Tray(appIcon())
+const Variant = {
+  Size16: { size: 16, scaleFactor: 1 },
+  Size32: { size: 32, scaleFactor: 2 },
+  Size48: { size: 48, scaleFactor: 3 },
+  Size256: { size: 256, scaleFactor: 16 },
+} as const;
 
-  tray.setToolTip(productName)
+const Variants = Object.values(Variant);
+
+function getDisplaysMaxScaleFactor(): number {
+  const displays = screen.getAllDisplays();
+  const scaleFactors = displays
+    .map(display => display.scaleFactor)
+    .filter(scaleFactor => Number.isFinite(scaleFactor) && scaleFactor > 1.0);
+  return Math.max(1.0, ...scaleFactors);
+}
+
+function getVariantForScaleFactor(scaleFactor: number) {
+  const match = Variants.find(variant => {
+    return variant.scaleFactor >= scaleFactor;
+  });
+
+  return match ?? Variant.Size32;
+}
+
+function getTrayIconPath(size: number): string {
+  let dirName = 'other'
+  const fileName = `icon-${size}x${size}.png`
+
+  if (platform.isMacOS) dirName = 'mac'
+
+  return join(resourcesPath, 'assets', 'tray', dirName, fileName)
+}
+
+function getTrayIcon(): NativeImage {
+  let image: NativeImage
+
+  if (platform.isLinux) {
+    // Linux: Static tray icons
+    // Use a single tray icon for Linux, as it does not support scale factors.
+    // We choose the best icon based on the highest display scale factor.
+    const scaleFactor = getDisplaysMaxScaleFactor()
+    const variant = getVariantForScaleFactor(scaleFactor)
+    const iconPath = getTrayIconPath(variant.size)
+    const buffer = readFileSync(iconPath)
+    
+    image = nativeImage.createFromBuffer(buffer, {
+      scaleFactor: 1.0,
+      width: variant.size,
+      height: variant.size,
+    })
+  } else {
+    // Windows/macOS: Responsive tray icons
+    image = nativeImage.createEmpty()
+
+    for (const variant of Variants) {
+      const iconPath = getTrayIconPath(variant.size)
+      const buffer = readFileSync(iconPath)
+      image.addRepresentation({
+        buffer,
+        width: variant.size,
+        height: variant.size,
+        scaleFactor: variant.scaleFactor,
+      })
+    }
+
+    // Set image as a Template Image for macOS.
+    if (platform.isMacOS) image.setTemplateImage(true)
+  }
+
+  return image
+}
+
+export function createTray() {
+  const trayIcon = getTrayIcon()
+  tray = new Tray(trayIcon)
+
+  if (!platform.isMacOS) {
+    tray.setToolTip(productName)
+  }
+  
   updateTray()
 
   tray.on('click', () => {
@@ -97,7 +177,8 @@ export function updateTray(title?: string) {
     },
   ])
 
-  tray.setToolTip(trayTooltip)
-  tray.setTitle(productName)
+  if (!platform.isMacOS) {
+    tray.setToolTip(trayTooltip)
+  }
   tray.setContextMenu(contextMenu)
 }
