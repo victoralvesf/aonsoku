48ae66a1e075a7068715b863b36dbcaa9d60b517
Kerenon <code@kerenon.com>
2025-09-15 15:47:04 +0200

    Send NowPlaying notification on playback start

--- src/service/scrobble.ts
+++ src/service/scrobble.ts
@@ -2,11 +2,12 @@ import { httpClient } from '@/api/httpClient'
 import { SubsonicResponse } from '@/types/responses/subsonicResponse'
 import dateTime from '@/utils/dateTime'
 
-async function send(id: string) {
+async function send(id: string, submission: boolean = true) {
   await httpClient<SubsonicResponse>('/scrobble', {
     method: 'GET',
     query: {
       id,
+      submission: submission ? 'true' : 'false',
       time: dateTime().valueOf().toString(),
     },
   })
