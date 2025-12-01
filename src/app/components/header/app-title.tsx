99ff5a4f7d41095edd41d02eaf66a268aae89217
Victor Alves <viictor.alvess12@gmail.com>
2025-12-01 00:43:58 -0300

    feat: add app dynamic icon to header

--- src/app/components/header/app-title.tsx
+++ src/app/components/header/app-title.tsx
@@ -1,10 +1,9 @@
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import appIcon from '@/assets/icon_transparent.svg'
+import { AppIcon } from '@/app/components/app-icon'
 
 export function AppTitle() {
   return (
     <div className="flex gap-2 items-center">
-      <LazyLoadImage src={appIcon} alt="Aonsoku icon" className="size-6" />
+      <AppIcon size={24} />
       <span className="leading-7 text-sm font-medium text-muted-foreground">
         Aonsoku
       </span>
