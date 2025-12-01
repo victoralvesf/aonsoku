1ce74f3b366003558d4d33972da4628d7365b3e3
Victor Alves <viictor.alvess12@gmail.com>
2025-11-30 23:41:56 -0300

    feat: update about dialog including dynamic app logo

--- src/app/components/about/dialog.tsx
+++ src/app/components/about/dialog.tsx
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/react-query'
 import { useTranslation } from 'react-i18next'
+import { AppIcon } from '@/app/components/app-icon'
+import { MultiBadge } from '@/app/components/ui/badge'
 import {
   Dialog,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from '@/app/components/ui/dialog'
-import roundedIcon from '@/assets/icon.svg'
 import githubIcon from '@/assets/icons/github-mark-white.svg'
 import { subsonic } from '@/service/subsonic'
 import { getAppInfo } from '@/utils/appName'
@@ -34,61 +36,44 @@ export function AboutDialog({ open, onOpenChange }: AboutDialogProps) {
       >
         <DialogTitle className="sr-only">{t('menu.about')}</DialogTitle>
         <DialogHeader>
-          <div className="flex gap-2 items-center justify-center w-full py-4 bg-gradient-to-b from-accent to-background">
-            <img
-              src={roundedIcon}
-              alt="Aonsoku"
-              className="size-12 drop-shadow grayscale"
-            />
-            <h1 className="font-semibold text-2xl drop-shadow">{name}</h1>
+          <div className="flex gap-2 items-center justify-start w-full py-4 px-6 bg-background-foreground border-b border-border">
+            <AppIcon className="size-8" />
+            <h1 className="font-medium text-lg">{name}</h1>
           </div>
         </DialogHeader>
 
-        <div className="w-full h-full p-6 gap-6 grid grid-cols-4">
-          <div className="flex flex-col gap-6 col-span-3 border-r">
+        <div className="w-full h-full p-6 gap-6 flex flex-col">
+          <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2 h-full text-sm">
-              <span className="font-medium">{t('about.client')}</span>
+              <span className="text-xs font-medium">{t('about.client')}</span>
               <div className="flex flex-col gap-1 justify-center text-muted-foreground">
                 <div className="flex gap-2">
-                  <p>{t('about.version')}</p>
-                  <div className="text-xs font-medium bg-primary/60 text-foreground px-2 rounded-full border border-primary flex items-center justify-center">
-                    {version}
-                  </div>
+                  <MultiBadge label={t('about.version')}>{version}</MultiBadge>
                 </div>
               </div>
             </div>
 
             <div className="flex flex-col gap-2 h-full text-sm">
-              <span className="font-medium">{t('about.server')}</span>
+              <span className="text-xs font-medium">{t('about.server')}</span>
               {isLoading && <p>{t('generic.loading')}</p>}
               {server && !isLoading && (
-                <div className="flex flex-col gap-1 justify-center text-muted-foreground">
-                  <div className="flex gap-2">
-                    <div>{t('about.type')}</div>
-                    <div className="text-xs font-medium bg-primary/60 text-foreground px-2 rounded-full border border-primary flex items-center justify-center">
-                      {server.type}
-                    </div>
-                  </div>
-                  <div className="flex gap-2">
-                    <div>{t('about.version')}</div>
-                    <div className="text-xs font-medium bg-primary/60 text-foreground px-2 rounded-full border border-primary flex items-center justify-center">
-                      {server.serverVersion}
-                    </div>
-                  </div>
-                  <div className="flex gap-2">
-                    <div>{t('about.apiVersion')}</div>
-                    <div className="text-xs font-medium bg-primary/60 text-foreground px-2 rounded-full border border-primary flex items-center justify-center">
-                      {server.version}
-                    </div>
-                  </div>
+                <div className="flex gap-2 flex-wrap">
+                  <MultiBadge label={t('about.type')}>{server.type}</MultiBadge>
+                  <MultiBadge label={t('about.version')}>
+                    {server.serverVersion}
+                  </MultiBadge>
+                  <MultiBadge label={t('about.apiVersion')}>
+                    {server.version}
+                  </MultiBadge>
                 </div>
               )}
             </div>
           </div>
-
-          <div>
+        </div>
+        <DialogFooter className="w-full border-t border-border px-6 py-4 bg-background-foreground">
+          <div className="flex justify-end">
             <a
-              className="w-full px-2 py-1 rounded-md bg-primary/60 hover:bg-primary/50 border border-primary text-sm font-medium flex items-center justify-center"
+              className="px-2 py-1 rounded-md bg-primary/60 hover:bg-primary/50 text-primary-foreground border border-primary/80 text-sm font-medium flex items-center justify-center"
               href={url}
               target="_blank"
               rel="nofollow noreferrer"
@@ -97,7 +82,7 @@ export function AboutDialog({ open, onOpenChange }: AboutDialogProps) {
               Github
             </a>
           </div>
-        </div>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   )
