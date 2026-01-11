e738b977bdc222650fb1b94d54bb9c8c42f31ea1
Vladimir Kruss <riddlah@yandex.ru>
2026-01-11 12:41:28 +0300

    fix: force enable pabm server parameters

---
+++ public/env-config.js
@@ -0,0 +1,8 @@
+"use strict";
+window.SERVER_URL="https://radiopabm.com" || undefined;
+window.HIDE_SERVER="true" === "true";
+window.APP_USER="" || undefined;
+window.APP_PASSWORD="" || undefined;
+window.APP_AUTH_TYPE="" || "token";
+window.HIDE_RADIOS_SECTION="true" === "true";
+window.SERVER_TYPE="navidrome" || undefined;
