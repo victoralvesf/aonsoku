1f0939541876a4a3c822f3d4ac38b68d0a7cd2ab
Victor Alves <viictor.alvess12@gmail.com>
2026-01-11 21:07:26 -0300

    chore: change vite base url config to support subdirectories

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { createManualChunks } from './src/manual-chunks'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
