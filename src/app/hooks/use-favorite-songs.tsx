a1ab2fcc6cf2f82cac89b5aa0d1d7ee78fe47b9e
Victor Alves <viictor.alvess12@gmail.com>
2026-01-13 20:05:57 -0300

    fix: remove staleTime and gcTime from use favorite songs query

--- src/app/hooks/use-favorite-songs.tsx
+++ src/app/hooks/use-favorite-songs.tsx
@@ -1,13 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { getFavoriteSongs } from '@/queries/songs'
-import { convertMinutesToMs } from '@/utils/convertSecondsToTime'
 import { queryKeys } from '@/utils/queryKeys'
 
 export function useFavoriteSongs() {
   return useQuery({
     queryKey: [queryKeys.favorites.songs],
     queryFn: getFavoriteSongs,
-    staleTime: convertMinutesToMs(5),
-    gcTime: convertMinutesToMs(5),
   })
 }
