bf3667a9fb939924e2743646c4d9ac1871ed5bf4
Vladimir Kruss <riddlah@yandex.ru>
2026-01-17 00:25:13 +0300

    branding: pabm icon on header

--- src/app/components/app-icon.tsx
+++ src/app/components/app-icon.tsx
@@ -1,18 +1,16 @@
 import { SVGProps } from 'react'
-import { cn } from '@/lib/utils'
 
 type AppIconProps = SVGProps<SVGSVGElement> & {
   size?: number
 }
 
 export function AppIcon({ size = 48, className, ...props }: AppIconProps) {
-  const strokeSize = 2
-
   return (
     <img
       src="/icon.svg"
       height="32"
       width="32"
+      alt="pabm"
     />
   )
 }
