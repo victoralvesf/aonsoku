c43ffa4e5b5a967355c1972b9079ac7e3b968b02
Victor Alves <39206163+victoralvesf@users.noreply.github.com>
2026-08-12 23:33:28 -0300

    chore: update manual chunks distribution

--- src/manual-chunks.ts
+++ src/manual-chunks.ts
@@ -11,10 +11,11 @@ export function createManualChunks(id: string) {
 
     const scopedPackageName = modulePath.split('/')[1]
     const includes = (id: string) => scopedPackageName.includes(id)
+    const includesAny = (ids: string[]) => ids.some(includes)
 
     if (includes('i18n')) return 'i18n'
     if (includes('tailwind')) return 'tailwind'
-    if (includes('html-to-text') || includes('linkify-it')) return 'formatters'
+    if (includesAny(['html-to-text', 'linkify-it'])) return 'formatters'
     if (includes('lucide')) return 'lucide'
     if (includes('embla')) return 'embla'
     if (includes('dayjs')) return 'date-time'
@@ -23,15 +24,25 @@ export function createManualChunks(id: string) {
     if (includes('lodash')) return 'lodash'
     if (includes('tanstack')) return 'tanstack'
     if (includes('radix')) return 'radix'
-    if (includes('markdown') || includes('remark') || includes('rehype'))
+    if (includesAny(['markdown', 'remark', 'rehype']))
       return 'markdown'
-    if (includes('react-hook-form') || includes('zod')) return 'forms'
+    if (includesAny(['react-hook-form', 'zod'])) return 'forms'
+    if (includes('dompurify')) return 'sanitizer'
+    if (includesAny(['zustand', 'immer', 'use-sync-external-store']))
+      return 'state'
+    if (includesAny(['fast-average-color', 'idb-keyval', 'iso-639-2']))
+      return 'misc'
     if (vendor.some((name) => scopedPackageName.startsWith(name))) {
       return 'vendor-dom'
     }
     if (scopedPackageName.startsWith('react')) return 'vendor'
 
     return undefined
+  }
+
+  if (id.includes('/src/i18n/locales/')) {
+    const lang = id.split('/src/i18n/locales/')[1].replace('.json', '')
+    return `locale-${lang}`
   }
 
   return undefined
