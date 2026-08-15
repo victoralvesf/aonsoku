62bcf0c639937fcb984c5f1833d00dd9ff1ca411
Victor Alves <39206163+victoralvesf@users.noreply.github.com>
2026-08-15 00:10:23 -0300

    feat: add widget config to electron vite build

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -49,6 +49,7 @@ export default defineConfig({
       rollupOptions: {
         input: {
           index: resolve(__dirname, 'index.html'),
+          widget: resolve(__dirname, 'widget.html'),
         },
         output: {
           manualChunks: createManualChunks,
